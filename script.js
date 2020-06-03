e0edcb22f1e451e501731ce90618b9e6486b6d3a
Azrielle <azriellepelayo@gmail.com>
2020-06-03 15:51:11 +0800

    Made functioning buttons

--- script.js
+++ script.js
@@ -1,11 +1,61 @@
-let numberOfRounds = +prompt("How many rounds do you want?");
 
-game(numberOfRounds);
+
+let playerChoice = document.querySelectorAll("button");
+
+playerChoice.forEach((button) =>{
+  button.addEventListener('click', event =>{
+    let playerSelection = button.id;
+    playRound(playerSelection, computerSelection);
+  })
+})
+
+
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "scissors")
+    return playerWins();
+
+  if (playerSelection == "paper" && computerSelection == "rock")
+    return playerWins();
+
+  if (playerSelection == "scissors" && computerSelection == "paper")
+    return playerWins();
+
+  if (playerSelection == computerSelection) return gameIsDraw();
+  else return playerLose();
+
+  function playerWins() {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    return 1;
+  }
+
+  function playerLose() {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+    return -1;
+  }
+
+  function gameIsDraw() {
+    console.log("Draw!");
+    return 0;
+  }
+}
+
+
+let choices = ["rock", "paper", "scissors"];
+function computerPlay(arrayOfChoices) {
+  let computerPick =
+    arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)];
+  return computerPick;
+}
+let computerSelection = computerPlay(choices);
 
 //game function accepts the number of rounds to be looped
 function game(numberOfRounds) {
   //Create an array for the computer's choices
-  let choices = ["rock", "paper", "scissors"];
+  
 
   /*roundStatus counts the number of wins. 
   If it is positive, the player wins. 
@@ -16,7 +66,7 @@ function game(numberOfRounds) {
   //Loop the number of rounds
   for (let i = 0; i < numberOfRounds; i++) {
     let playerSelection = prompt("Rock, paper, or scissors? ").toLowerCase();
-    let computerSelection = computerPlay(choices);
+
 
     // If the player has a wrong input, the game stops.
     if (!choices.includes(playerSelection))
@@ -31,38 +81,7 @@ function game(numberOfRounds) {
   else console.log("Game over. It's a draw!");
 
   // Create a function that lets the computer randomly choose a choice
-  function computerPlay(arrayOfChoices) {
-    let computerPick =
-      arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)];
-    return computerPick;
-  }
+  
 
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "scissors")
-      return playerWins();
-
-    if (playerSelection == "paper" && computerSelection == "rock")
-      return playerWins();
-
-    if (playerSelection == "scissors" && computerSelection == "paper")
-      return playerWins();
-
-    if (playerSelection == computerSelection) return gameIsDraw();
-    else return playerLose();
-
-    function playerWins() {
-      console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-      return 1;
-    }
-
-    function playerLose() {
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-      return -1;
-    }
-
-    function gameIsDraw() {
-      console.log("Draw!");
-      return 0;
-    }
-  }
+  
 }
