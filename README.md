dbe564d244834dfd81386adbd667a85597d6a4d4
Azrielle <azriellepelayo@gmail.com>
2020-06-04 22:04:49 +0800

    - Changed the name gameResult into resultContainer in index.html - Made the text content of button into uppercase in style.css - Changed function names in script.js  to make it more self-documenting - Removed all console.log lines - Fixed the order of functions based on game function - Transferred startGame, gameOver, playerWins, playerLose, and gameIsDraw outside of game and playRound

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Rock Paper Scissors Game
 This is a practice project from the **Web Development 101** course of **The Odin Project**.
+
+## version 1
+
+- Done on **05/31/2020**
+
+- JavaScript file for the first version of the game is in `script_v1.js` 
+- What's inside:
+  - A `prompt` that lets the player input the number of rounds and the choice of rock, paper, or scissors
+  - The result for each round and the winner is logged in the console 
+
+
+
+## version 2
+
+- Done on **06/04/2020**
+- JavaScript file is in `script.js`
+- What's inside:
+  - A `click` event that starts the game when the player clicks the choice of rock, paper, or scissors
+  - Displayed hand is defaulted to rock and changes when the player and computer picks a choice
+  - Game runs on 5 rounds. After 5 rounds, a modal appears displaying the results of the game and a Play Again button
+  - Play Again button refreshes the page
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let playerChoice = document.querySelector(".choices");
+let playerClick = document.querySelector(".choices");
 let playerHand = document.querySelector(".playerHand");
 let computerHand = document.querySelector(".computerHand");
 let playerCurrentScore = document.querySelector(".playerCurrentScore");
@@ -9,47 +9,49 @@ let roundResult = document.querySelector(".roundResult");
 let modal = document.querySelector(".modal");
 let gameResult = document.querySelector(".result");
 let playAgain = document.querySelector(".playAgain");
+
 let playerScore = 0;
 let computerScore = 0;
 
 game(5);
 
 function game(numberOfRounds) {
-  function displayResult() {
-    modal.style.visibility = "visible";
-    if (playerScore > computerScore) {
-      gameResult.textContent = "You Win!";
-      gameResult.style.color = "#03dac5";
-    } else if (playerScore == computerScore) {
-      gameResult.textContent = "It's a tie!";
-      gameResult.style.color = "#FFFFFF";
-    }
-    playAgain.addEventListener("click", () => {
-      location.reload();
-      return false;
-    });
-  }
-
   let clickTimes = 0;
-  playerChoice.addEventListener("click", g);
-  function g(event) {
-    if (event.target !== event.currentTarget) {
-      let playerSelection = event.target.textContent;
-      let computerSelection = computerPlay(choices);
-      playRound(playerSelection, computerSelection);
-      clickTimes++;
-      if (clickTimes == numberOfRounds) {
-        displayResult();
-      }
-    }
+  let choices = ["rock", "paper", "scissor"];
+  playerClick.addEventListener("click", startGame);
+
+  function startGame(event) {
+    let playerSelection = event.target.textContent;
+    let computerSelection = computerPlay(choices);
+
+    playRound(playerSelection, computerSelection);
+    clickTimes++;
+    if (clickTimes == numberOfRounds) {
+      gameOver();
+    } 
   }
 }
 
-let choices = ["rock", "paper", "scissor"];
 function computerPlay(arrayOfChoices) {
   let computerPick =
     arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)];
   return computerPick;
+}
+
+function gameOver() {
+  modal.style.visibility = "visible";
+  if (playerScore > computerScore) {
+    gameResult.textContent = "You Win!";
+    gameResult.style.color = "#03dac5";
+  } 
+  else if (playerScore == computerScore) {
+    gameResult.textContent = "It's a tie!";
+    gameResult.style.color = "#FFFFFF";
+  }
+  playAgain.addEventListener("click", () => {
+    location.reload();
+    return false;
+  });
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -67,24 +69,20 @@ function playRound(playerSelection, computerSelection) {
   } else {
     return playerLose();
   }
+}
 
-  function playerWins() {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-    roundResult.textContent = "YOU WIN";
-    playerScore++;
-    playerCurrentScore.textContent = playerScore;
-  }
+function playerWins() {
+  roundResult.textContent = "YOU WIN";
+  playerScore++;
+  playerCurrentScore.textContent = playerScore;
+}
 
-  function playerLose() {
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-    roundResult.textContent = "YOU LOSE";
+function playerLose() {
+  roundResult.textContent = "YOU LOSE";
+  computerScore++;
+  computerCurrentScore.textContent = computerScore;
+}
 
-    computerScore++;
-    computerCurrentScore.textContent = computerScore;
-  }
-
-  function gameIsDraw() {
-    roundResult.textContent = "DRAW";
-    console.log("Draw");
-  }
+function gameIsDraw() {
+  roundResult.textContent = "DRAW";
 }
